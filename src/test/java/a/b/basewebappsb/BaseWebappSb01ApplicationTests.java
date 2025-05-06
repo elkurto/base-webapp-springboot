8d5fc7c4359ac39bc31fd2dd303fa66e77d21f4f
elkurto <kurt.buehner@gmail.com>
2025-05-06 19:53:49 -0400

    initial - base springboot webapp  using openjdk21

---
+++ src/test/java/a/b/basewebappsb/BaseWebappSb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package a.b.basewebappsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseWebappSb01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
