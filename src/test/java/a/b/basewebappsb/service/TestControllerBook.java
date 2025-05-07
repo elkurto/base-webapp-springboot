698dbc0ea6beaf0ffd839ae5120773c05f55b7a1
elkurto <kurt.buehner@gmail.com>
2025-05-06 20:08:38 -0400

    added @WebMvcTest(ControllerBook.class)

--- src/test/java/a/b/basewebappsb/service/TestControllerBook.java
+++ src/test/java/a/b/basewebappsb/service/TestControllerBook.java
@@ -1,4 +1,30 @@
 package a.b.basewebappsb.service;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.
+        MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.
+        MockMvcResultMatchers.status;
+@WebMvcTest(ControllerBook.class)
 public class TestControllerBook {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testIndex() throws Exception {
+        var expected = "This is the /book controller";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString(expected)))
+                .andExpect(content().contentTypeCompatibleWith( MediaType.TEXT_PLAIN));
+
+    }
 }
