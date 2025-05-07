696c6c334319a785bfb1e97d97aae35db2340816
elkurto <kurt.buehner@gmail.com>
2025-05-07 07:19:05 -0400

    implemented ServiceBook.java

--- src/main/java/a/b/basewebappsb/service/ServiceBook.java
+++ src/main/java/a/b/basewebappsb/service/ServiceBook.java
@@ -7,7 +7,22 @@ import java.util.*;
 
 @Service
 public class ServiceBook {
-    static Map<UUID,Book> mapIdBook =new HashMap<>();
+    private static final Map<UUID,Book> mapIdBook =new HashMap<>();
+    static {
+        final long now_epoch_seconds =System.currentTimeMillis() / 1000;
+        final List<Book> listBook = new ArrayList<>();
+        listBook.add( new Book(UUID.randomUUID(), "Docker Deep Dive", "Poulton", now_epoch_seconds));
+        listBook.add( new Book(UUID.randomUUID(), "Art Of War", "Tsu", now_epoch_seconds));
+        listBook.add( new Book(UUID.randomUUID(), "The Nail", "Davis,Farmer", now_epoch_seconds));
+        listBook.add( new Book(UUID.randomUUID(), "Art Of Webassembly", "Battagline", now_epoch_seconds));
+        listBook.add( new Book(UUID.randomUUID(), "1984", "Orwell", now_epoch_seconds));
+        listBook.add( new Book(UUID.randomUUID(), "Python 3", "Ernesti,Kaiser", now_epoch_seconds));
+        listBook.add( new Book(UUID.randomUUID(), "ALG & DS in Python", "Canning,Broder,Lafore", now_epoch_seconds));
+
+        for (Book book : listBook) {
+            mapIdBook.put(book.id, book);
+        }
+    }
 
     public ServiceBook() {}
 
@@ -20,14 +35,19 @@ public class ServiceBook {
         }
     }
 
-    public void delete(Book book) {
-        if ( book != null && book.id != null ) {
+    public void removeById(UUID id) {
+        if ( book.id != null ) {
             mapIdBook.remove(book.id);
+        }
+    }
+    public void remove(Book book) {
+        if ( book != null ) {
+            this.removeById(book.id);
         }
     }
 
     public Book get(UUID id) {
-        book = mapIdBook.getOrDefault(id, null);
+        return mapIdBook.getOrDefault(id, null);
     }
 
     public List<Book> getAll() {
