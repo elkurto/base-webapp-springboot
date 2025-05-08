8fd97f35d37d5e5fd51e9f782f7e07837e6a9a93
elkurto <kurt.buehner@gmail.com>
2025-05-08 13:09:56 -0400

    updated MockBean to MockitoBean

--- src/test/java/a/b/basewebappsb/service/TestControllerBook.java
+++ src/test/java/a/b/basewebappsb/service/TestControllerBook.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.boot.test.mock.mockito.MockitoBean;
 
 import static org.springframework.test.web.servlet.result.
         MockMvcResultMatchers.content;
@@ -21,7 +22,7 @@ public class TestControllerBook {
     private MockMvc mockMvc;
     private final ObjectMapper mapper = new ObjectMapper();
 
-    @MockBean
+    @MockitoBean
     private ServiceBook serviceBook;
 
     @Test
