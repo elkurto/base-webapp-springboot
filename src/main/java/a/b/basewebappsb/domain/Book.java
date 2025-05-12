7f09337d317b96606cee2647323143343a26ab46
elkurto <kurt.buehner@gmail.com>
2025-05-12 04:55:35 -0400

    Book.java ::equal and ::hashcode

--- src/main/java/a/b/basewebappsb/domain/Book.java
+++ src/main/java/a/b/basewebappsb/domain/Book.java
@@ -1,6 +1,8 @@
 package a.b.basewebappsb.domain;
 
+import java.util.Objects;
 import java.util.UUID;
+
 
 public class Book {
     public UUID id;
@@ -17,5 +19,18 @@ public class Book {
         this.name =name;
         this.author = author;
         this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(this.id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
     }
 }
